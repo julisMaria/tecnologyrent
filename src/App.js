62474f95af2bdb89a4d8e8a33f27805e43ccfd19
Carolina Meneses <xmeneses03@hotmail.com>
2022-12-09 18:15:42 -0500

    Componente mostrar creado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+//importamos componentes
+import CompMostrarProveedores from './Componentes/Proveedores/MostrarProveedores';
+//importamos librerias routes
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <button type="button" class="btn btn-warning">Warning</button>
-        </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<CompMostrarProveedores/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+//primero se inicia el backend
--- src/Componentes/Proveedores/mostrarProveedores.js
+++ src/Componentes/Proveedores/mostrarProveedores.js
@@ -4,25 +4,62 @@ import { Link } from 'react-router-dom';
 
 const URL = 'http://localhost:5000/api/proveedores/'
 
-const compMostrarProveedores = () => {
-    const [proveedores, setProveedor] = useState([])
+const CompMostrarProveedores = () => {
+    const [proveedores, setProveedor] = useState([]);
     useEffect(() => {
         getProveedores()
-    }, []);
+    },[]);
 
-const getProveedores = async () => {
-    const resul = await axios.get(URL)
-    setProveedor(resul.data);
+    const getProveedores = async () => {
+        const res = await axios.get(URL);
+        setProveedor(res.data);
+    }
+
+    //console.log('estoy en la rama secundaria')
+
+    const eliminarProveedores = async (id) => {
+        await axios.delete(`${URL}${id}`)
+        getProveedores();
+    }
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <Link to='/agregar' className='btn btn-primary mt-2 mb-2'><i className="fa-solid fa-user-plus"></i></Link>   
+                    <table className='table'>
+                        <thead className='tableTheadBg'>
+                            <tr>                           
+                                <th> Nombres </th>
+                                <th> Apellidos </th>
+                                <th> Documento </th>
+                                <th> Correo </th>
+                                <th> Telefono </th>
+                                <th> Empresa </th>
+                                <th> Acciones </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {proveedores.map((proveedor, index) => (
+                                <tr key={index}>
+                                    <td>{proveedor.nombres}</td>
+                                    <td>{proveedor.apellidos}</td>
+                                    <td>{proveedor.documento}</td>
+                                    <td>{proveedor.correo}</td>
+                                    <td>{proveedor.telefono}</td>
+                                    <td>{proveedor.empresa}</td>
+                                    <td> 
+                                        <Link to={`/editar/${proveedor._id}`} className='btn btn-info'><i className="fa-solid fa-file-pen"></i></Link> 
+                                        <button onClick={() => eliminarProveedores(proveedor._id)} className='btn btn-danger'><i className="fa-solid fa-user-xmark"></i></button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-console.log('estoy en la rama secundaria')
-
-const eliminarProveedores = async(id) => {
-    await axios.delete(`${URL}${id}`)
-    getProveedores()
-}
-
-
-}
-
-export default compMostrarProveedores;
+export default CompMostrarProveedores;

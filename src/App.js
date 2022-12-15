1e0ecbc84ef9209a6786452d0c2e56c437625389
Carolina Meneses <xmeneses03@hotmail.com>
2022-12-14 19:57:36 -0500

    diseño de menu agregado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 //importamos componentes
-import {CompMostrarProveedores, CompAgregarProveedores, CompEditarProveedores, 
-      CompMostrarClientes, CompAgregarClientes, CompEditarClientes, 
-      CompMostrarProductos, CompEditarProductos, CompAgregarProductos, 
-      CompMostrarSolicitudes, CompAgregarSolicitudes, CompEditarSolicitud} 
-from './Componentes/index.js';
+import {
+  CompMostrarProveedores, CompAgregarProveedores, CompEditarProveedores,
+  CompMostrarClientes, CompAgregarClientes, CompEditarClientes,
+  CompMostrarProductos, CompEditarProductos, CompAgregarProductos,
+  CompMostrarSolicitudes, CompAgregarSolicitudes, CompEditarSolicitud
+}
+  from './Componentes/index.js';
 
 //importamos librerias routes
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -13,6 +15,22 @@ function App() {
   return (
     <div className="App">
       <header className="container">
+        <nav className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+
+          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <img src="https://misiontic2022.mintic.gov.co/mt2022/assets/assets/images/logo-mision.png" style={{ height: 32 }} alt='logo' />
+            <span className="fs-4">Website</span>
+          </a>
+
+          <ul className="nav nav-pills">
+            <li className="nav-item"><a href="/" className="nav-link active">Inicio</a></li>
+            <li className="nav-item"><a href="/productos" className="nav-link">Productos</a></li>
+            <li className="nav-item"><a href="/clientes" className="nav-link ">Clientes</a></li>
+            <li className="nav-item"><a href="/proveedores" className="nav-link">Proveedores</a></li>
+            <li className="nav-item"><a href="/solicitudes" className="nav-link">Solicitudes</a></li>
+          </ul>
+
+        </nav>
 
       </header>
       <BrowserRouter>

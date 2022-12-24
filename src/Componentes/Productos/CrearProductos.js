3abeafffd8014f94066c4849b24f2566fec1475a
julisMaria <113642048+julisMaria@users.noreply.github.com>
2022-12-23 20:54:57 -0500

    Update CrearProductos.js

--- src/Componentes/Productos/CrearProductos.js
+++ src/Componentes/Productos/CrearProductos.js
@@ -2,7 +2,7 @@ import axios from 'axios';  //sirve para hacer peticiones
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const URL = 'http://localhost:5000/api/productos/'
+const URL = 'http://150.136.34.191:5000/api/productos/'
 
 export const CompAgregarProductos = () => {
     //propiedades del modelo

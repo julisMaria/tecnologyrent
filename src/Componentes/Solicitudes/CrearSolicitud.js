c7d498f356c884edc9effd8059a9ec674e2838f1
julisMaria <113642048+julisMaria@users.noreply.github.com>
2022-12-23 20:57:25 -0500

    Update CrearSolicitud.js

--- src/Componentes/Solicitudes/CrearSolicitud.js
+++ src/Componentes/Solicitudes/CrearSolicitud.js
@@ -2,7 +2,7 @@ import axios from 'axios';  //sirve para hacer peticiones
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const URL = 'http://localhost:5000/api/solicitudes/'
+const URL = 'http://150.136.34.191:5000/api/solicitudes/'
 
 export const CompAgregarSolicitudes = () => {
     //propiedades del modelo
